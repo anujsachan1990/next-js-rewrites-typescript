bf9dfff15a5de11a2938dbc0b44b3cdb1f2af8f9
Anuj Sachan <anuj.sachan@akqa.com>
2022-09-05 16:14:59 +1000

    added user agent

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'User-Agent',
+            value: 'CRGAPP-AU-CR',
+          },
+        ],
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
